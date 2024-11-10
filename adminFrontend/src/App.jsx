a33c479678ff30cb686327ab85545d714ea78b5b
karusk22 <karunyansk22@gmail.com>
2024-11-10 23:30:11 +0530

    Karunyan : Added some components in user page

--- adminFrontend/src/App.jsx
+++ adminFrontend/src/App.jsx
@@ -1,11 +1,14 @@
 
 import Order from "./Component/order_info"
+import Admin_home from "./Component/admin_home"
+import Admin_supplier from "./Component/admin_supplier"
+import Admin_login from "./Component/admin_login"
 function App() {
 
 
   return (
     <>
-      <Order/>
+      <Admin_login/>
     </>
   )
 }
